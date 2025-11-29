820a0621be70551ed460b587479c739bf1d89271
Eugene <zhenya.androshchuk@gmail.com>
2025-11-29 22:46:26 +0200

    Update

--- main.py
+++ main.py
@@ -5,15 +5,24 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))) 
 
-from core.config import dp, bot, logger 
-from core.mu import song_data_storage 
+from core.config import dp, bot, logger, CONCURRENT_DOWNLOAD_LIMIT 
+from core.services.storage import song_data_storage 
 
-from core import mu 
+from core.handlers import messages, callbacks 
 
-if __name__ == "__main__":
+
+async def main():
     if not bot.token:
         logger.error("BOT_TOKEN is not set in the .env file. The bot cannot start.")
-    else:
-        logger.info(f"Start polling")
-        logger.info(f"Loaded {len(song_data_storage)} cached songs.")
-        asyncio.run(dp.start_polling(bot))
+        return 
+
+    logger.info(f"Start polling")
+    logger.info(f"Loaded {len(song_data_storage)} cached songs.")
+    
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.warning("Bot stopped!")
