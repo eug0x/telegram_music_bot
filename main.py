8075857fad080874477130c96ddff1d8701051d4
Eugene <zhenya.androshchuk@gmail.com>
2025-12-03 04:44:47 +0200

    Patch

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import asyncio
 import os
 import logging
 import sys
+import aiohttp
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))) 
 
 from core.config import dp, bot, logger, CONCURRENT_DOWNLOAD_LIMIT 
-from core.services.storage import song_data_storage 
+from core.services.storage import song_data_storage
+from core.services.youtube import GLOBAL_HTTP_SESSION, close_global_session
 
 from core.handlers import messages, callbacks 
 
 
 async def main():
+    global GLOBAL_HTTP_SESSION
+    if GLOBAL_HTTP_SESSION is None:
+        GLOBAL_HTTP_SESSION = aiohttp.ClientSession()
+    
     if not bot.token:
         logger.error("BOT_TOKEN is not set in the .env file. The bot cannot start.")
         return 
@@ -19,7 +25,11 @@ async def main():
     logger.info(f"Start polling")
     logger.info(f"Loaded {len(song_data_storage)} cached songs.")
     
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await close_global_session()
+        logger.warning("Bot finished polling and closing session.")
 
 if __name__ == "__main__":
     try:
